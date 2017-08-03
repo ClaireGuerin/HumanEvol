74269a21ea3b59e35540b3e89d8cc370ff06f36e
Claire <guerin.claire01@gmail.com>
2017-08-03 13:17:07 +0200

    some changes

--- dietEvolposthoc.m
+++ dietEvolposthoc.m
@@ -8,12 +8,18 @@
 PathName = 'C:\Users\Claire\Dropbox\MEME\Montpellier\AdaptiveDynamicsStratification\pData';
 cd(PathName)
 
-fileList = dir(PathName);
+fileList = dir([PathName,'\*.mat']);
 nGen = 200000;
 nStrat = 100;
-fullMat = nan(nGen,nStrat,nStrat);
 
 nFiles = size(fileList);
+fileName = fileList(nFiles).name;
+lastGens = load(fileName);
+
+figure
+contourf(lastGens(end,:,:))
+
+fullDynamic = nan(nGen); % evolution of p for 1 strategy
 
 for file = 1:nFiles
     fileName = fileList(file).name;
